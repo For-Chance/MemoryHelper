f1616b112a34ea0f522bc73fb1af4c95bd04efb8
Chance <979569399@qq.com>
2022-07-02 21:01:36 +0800

    增加了可以使用正则表达式搜索与背诵的地方

--- helper.py
+++ helper.py
@@ -6,14 +6,14 @@ def helper():
     print("指令说明：")
     print("\th:\t帮助")
     print("\tq:\t退出")
-    print("\t+_+_:\t加上排序与问题")
-    print("\t-_:\t根据排序删除问题")
-    print("\ts:\t显示所有问题，与记忆次数")
+    print("\t+,_,_:\t加上排序与问题")
+    print("\t-,_:\t根据排序删除问题")
     print("\tsort:\t按index排序")
+    print("\ts,_:\t显示问题，后接正则表达式（可省略），与记忆次数")
     print("\tc:\t记忆库times清0")
     print("\tcc:\t删除记忆库")
     print("")
-    print("\tm_:\t开始记忆模式,后为记忆掩码")
+    print("\tm,_,_:\t开始记忆模式,使用正则表达式索引,和记忆掩码")
     print("\t\t按enter:\t下一条问题")
     print("\t\tqm:\t退出记忆模式")
     print("\t\tv:\t这个问题已经记住")
@@ -36,15 +36,18 @@ while 1:
         df.to_csv("df.csv")
         break
     elif order[0] == "+":
-        (index, question) = tuple(order[1:].split("+"))
+        (index, question) = tuple(order[2:].split(","))
         df.loc[index] = [question, 0]
     elif order[0] == "-":
-        index = order[1:]
+        index = order[2:]
         df = df.drop(index)
-    elif order == "s":
-        print(df)
     elif order == "sort":
         df = df.sort_index()
+    elif order[0] == "s":
+        regStr = ".*"
+        if len(order) > 2 and order[1] == ',':
+            regStr = order[2:]
+        print(df.filter(regex=regStr, axis=0))
     elif order == "c":
         df.times = 0
     elif order == "cc":
@@ -52,20 +55,26 @@ while 1:
     elif order[0] == "m":
         print("进入记忆模式！")
         mask = 0
-        if order == "m":
-            mask = 0
-        else:
-            mask = eval(order[1:])
+        regStr = ".*"
+        if len(order) > 2:
+            maskNreg = order[2:].split(',')
+            if len(maskNreg) == 1:
+                regStr = maskNreg[0] if maskNreg[0] != '' else ".*"
+            elif len(maskNreg) == 2:
+                regStr = maskNreg[0] if maskNreg[0] != '' else ".*"
+                mask = eval(maskNreg[1]) if maskNreg[1] != '' else 0
+            else:
+                print("Error:m指令出错!")
+                continue
         if len(df) == 0:
-            print("error:记忆库无内容！")
+            print("Error:记忆库无内容！")
             continue
         while 1:
-            if len(df[df["times"] <= mask]) == 0:
-                print("error:记忆库无满足条件的记忆内容！")
+            flt = df[df.times <= mask].filter(regex=regStr,axis=0)
+            if len(flt) == 0:
+                print("Error:记忆库无满足条件的记忆内容！")
                 break
-            qs = df.sample(n=1)
-            if qs.times.iloc[0] > mask:
-                continue
+            qs = flt.sample(n=1)
             print(f"?:\t{qs.question.iloc[0]}\t", end="")
             m_order = input()
             if m_order == "qm":
@@ -73,7 +82,5 @@ while 1:
                 break
             elif m_order == "v":
                 df.at[qs.index[0], "times"] += 1
-            elif m_order == "x":
-                continue
             else:
                 continue
